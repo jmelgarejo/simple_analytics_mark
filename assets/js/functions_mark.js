38315766f8bb9d91f80736c8b7a4ed3d14b45ee8
Juan Andres Melgarejo <juan.andres@puduhobbies.com>
2015-04-17 12:45:22 -0300

    order code and add license

--- assets/js/functions_mark.js
+++ assets/js/functions_mark.js
@@ -13,10 +13,9 @@ $(function(){
 		if(typeof data.trackevent !== 'undefined'){
 			var marcas = data.trackevent.split("/");
 			if (typeof ga !== 'undefined') {
-                   ga('send', 'event', marcas[0], marcas[1], marcas[2]);
-                }
-               //check if _gaq is set too
-
+                   		ga('send', 'event', marcas[0], marcas[1], marcas[2]);
+			}
+            //check if _gaq is set too
 			if(typeof _gaq !== 'undefined'){
 				_gaq.push(['_trackEvent', marcas[0], marcas[1], marcas[2]]);
 			}
@@ -24,9 +23,8 @@ $(function(){
 		if(typeof data.trackpage !== 'undefined'){
 			var marca = data.trackpage;
 			if (typeof ga !== 'undefined') {
-                   ga('send', 'pageview', marca);
-                }
-
+				ga('send', 'pageview', marca);
+			}
 			if(typeof _gaq !== 'undefined'){
 				_gaq.push(['_trackPageview', marca]);
 			}
